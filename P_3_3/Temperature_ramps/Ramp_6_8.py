2641c7fc59b9284750fe38a10947be35e99aa549
Emmanuel Calderón <39386880+joseema31416@users.noreply.github.com>
2019-11-27 10:36:19 -0600

    New Isobars

---
+++ P_3_3/Temperature_ramps/Ramp_6_8.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Np's and Mesogens, Temperature Ramp 6.6 -> 6.8
+
+# In[1]:
+
+
+#-----Import essential packages.
+
+from __future__ import division
+import hoomd
+import hoomd.md
+
+
+# In[2]:
+
+
+#-----Define important variables.
+
+p_max = 3.3;
+t_0   = 6.6; 
+t_1   = 6.8;
+steps_ramp = 5e4;
+
+
+# In[3]:
+
+
+#-----Declare the file from wich we'll extract the configuration of the system.
+
+ramp_file = "T_CM&NP_" + str(t_0) + "_P_" + str(p_max) + "_ramp.gsd"
+
+
+# In[4]:
+
+
+#-----Declare a simulation context.
+
+hoomd.context.initialize("--mode=cpu", memory_traceback = True);
+
+#-----Initialize from a .gsd file.
+
+system = hoomd.init.read_gsd(ramp_file, frame = -1);
+
+#-----Define each mesogen in the local frame of reference of each rigid body.
+
+rigid = hoomd.md.constrain.rigid();
+rigid.set_param('M', 
+                types = ['A']*8,
+                positions = [(-4,0,0),(-3,0,0),(-2,0,0),(-1,0,0),
+                           (1,0,0),(2,0,0),(3,0,0),(4,0,0)]);
+
+#-----Create the mesogens.
+
+rigid.create_bodies();
+
+#-----Define the potential energy.
+
+nl = hoomd.md.nlist.tree();
+
+lj = hoomd.md.pair.lj(r_cut = 3.5, nlist = nl)
+lj.set_params(mode = 'shift')
+
+#-----Declare interactions.
+
+lj.pair_coeff.set('NP','NP', epsilon = 1.0, sigma = 5.0);
+lj.pair_coeff.set('M' ,'M' , epsilon = 1.0, sigma = 1.0);
+lj.pair_coeff.set('A' ,'A' , epsilon = 1.0, sigma = 1.0);
+lj.pair_coeff.set('M' ,'A' , epsilon = 1.0, sigma = 1.0);
+lj.pair_coeff.set('NP','M' , epsilon = 1.0, sigma = 3.0);
+lj.pair_coeff.set('NP','A' , epsilon = 1.0, sigma = 3.0);
+
+#-----Select an standard integrator.
+
+hoomd.md.integrate.mode_standard(dt = 0.005);
+
+#------Define some groups and make their union.
+
+nanoparticles = hoomd.group.type(name = 'Nano_Particles', type = 'NP');
+mesogens = hoomd.group.rigid_center();
+groupNP_mes = hoomd.group.union(name = "NP_Mes", a = nanoparticles, b = mesogens);
+
+#----Integrate using an NPT integrator.
+
+temp = hoomd.variant.linear_interp(points = [(0,t_0), (steps_ramp, t_1)]);
+
+npt = hoomd.md.integrate.npt(group = groupNP_mes, kT = temp, tau = 2.0, tauP = 3.0, P = p_max);
+npt.randomize_velocities(seed=42)
+
+#-----Save .log y .gsd data.
+
+log_file = "T_" + str(t_1) + "_P_" + str(p_max) + "_ramp.log"
+gsd_file = "T_" + str(t_1) + "_P_" + str(p_max) + "_ramp.gsd" 
+meso_gsd_file = "T_CM&NP_" + str(t_1) + "_P_" + str(p_max) + "_ramp.gsd"
+
+log = hoomd.analyze.log(filename = log_file,
+                         quantities = ['num_particles',
+                                     'ndof',
+                                     'translational_ndof',
+                                     'rotational_ndof',
+                                     'potential_energy',
+                                     'kinetic_energy',
+                                     'translational_kinetic_energy',
+                                     'rotational_kinetic_energy',
+                                     'temperature',
+                                     'pressure',
+                                      'volume'],
+                         period = 1e3,
+                         overwrite = True);
+gsd = hoomd.dump.gsd(gsd_file,
+               period = 1e3,
+               group = hoomd.group.all(),
+               overwrite = True); 
+meso_gsd = hoomd.dump.gsd(meso_gsd_file,
+               period = 1e3,
+               group = groupNP_mes,
+               overwrite = True); 
+
+
+# In[ ]:
+
+
+#-----Run the simulation.
+
+hoomd.run(steps_ramp)
+
+
+# In[ ]:
+
+
+#-----Get density.
+
+system.get_metadata()
+
+
+# In[ ]:
+
+
+#-----Get volume.
+
+system.box.get_volume()
+
---
+++ P_3_3/Temperature_ramps/ramps_P_3_3.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#SBATCH --job_name=T_3_3_ramps
+#SBATCH -p gpus
+#SBATCH -n 1 #### 20
+#SBATCH --gres=gpu:2 ### 4 o 8
+#SBATCH --time 04-00:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=eq.emmanuel.137@gmail.com
+
+module load singularity/hoomd/2.3.5-CUDA
+
+hoomd.python3 Ramp_5_2.py
+hoomd.python3 Ramp_5_4.py
+hoomd.python3 Ramp_5_6.py
+hoomd.python3 Ramp_5_8.py
+hoomd.python3 Ramp_6_0.py
+hoomd.python3 Ramp_6_2.py
+hoomd.python3 Ramp_6_4.py
+hoomd.python3 Ramp_6_6.py
+hoomd.python3 Ramp_6_8.py
+hoomd.python3 Ramp_7_0.py
